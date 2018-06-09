83cb125b74379086ba5f0165860ec10776d98bbd
Nati <natanael.lup@gmail.com>
2018-06-09 16:03:45 +0300

    add recognition controller

---
+++ src/FeedbackBundle/Controller/RecognitionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FeedbackBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RecognitionController extends Controller
+{
+
+    public function listAction(Request $request)
+    {
+
+    }
+
+    public function addAction(Request $request)
+    {
+
+    }
+
+    public function editAction(Request $request)
+    {
+
+    }
+
+    public function removeAction(Request $request)
+    {
+
+    }
+}
