2c910df9a1ec7c37997e2224f3d2d77198b49a85
Nati <natanael.lup@gmail.com>
2018-07-10 18:48:07 +0300

    add chart to my feedback page

--- src/FeedbackBundle/Resources/public/js/BasicChart.js
+++ src/FeedbackBundle/Resources/public/js/BasicChart.js
@@ -39,19 +39,36 @@
             type: "get",
             success: function (response) {
                 var data = []; var labels = [];
+
                 $.each(response, function(key, value){
                     labels.push(key);
                     data.push(value);
                 });
-
                 var ctx = document.getElementById('base-chart-canvas').getContext('2d');
                 var myChart = new Chart(ctx, {
                     type: 'bar',
                     data: {
                         labels: labels,
                         datasets: [{
-                            label: "# of ability",
-                            data: data
+                            label: 'Rating from feedback',
+                            data: data,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
                         }]
                     },
                     options: {
@@ -67,8 +84,6 @@
             }
         });
     };
-
-
 
     $(document).ready(function(){
        new BaseChartController;
