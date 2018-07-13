8b5c938a8d95e061a38ff6df85a9ffdc7f1dd213
Nati <natanael.lup@gmail.com>
2018-07-13 13:37:34 +0300

    create change password page

---
+++ src/UserBundle/Controller/ChangePasswordController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+
+class ChangePasswordController extends \FOS\UserBundle\Controller\ChangePasswordController
+{
+    /**
+     * Generate the redirection url when the resetting is completed.
+     *
+     * @param \FOS\UserBundle\Model\UserInterface $user
+     *
+     * @return string
+     */
+    protected function getRedirectionUrl(UserInterface $user)
+    {
+        return $this->container->get('router')->generate('homepage');
+    }
+}
